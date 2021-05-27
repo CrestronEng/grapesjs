2f436d10e4d02895b384f17d60c9fcd149fc0abf
Kristina Corrado <kcorrado@crestron.com>
2021-05-27 11:12:08 -0400

    ci: added the equivalent code in node

--- scripts/build-locale.js
+++ scripts/build-locale.js
@@ -7,9 +7,6 @@ const copyRecursiveSync = (src, dest) => {
     const exists = fs.existsSync(src);
     const isDir = exists && fs.statSync(src).isDirectory();
 
-	//TODO - Adjust this to JavaScript
-    //rm -rf ./locale
-
     if (isDir) {
         fs.mkdirSync(dest);
         fs.readdirSync(src).forEach((file) => {
@@ -20,6 +17,7 @@ const copyRecursiveSync = (src, dest) => {
     }
 };
 
+fs.rmdirSync(localeDir, {recursive: true});
 copyRecursiveSync(localeSrcDir, localeDir);
 
 // Create locale/index.js file
