4aca9a6f197c7881758257c2b351d8e3ce90aafb
Olufemi Olumaiyegun <olufemi.olumaiyegun@crestron.com>
2021-07-08 17:17:08 -0400

    fix: make drag positioning sticky to stop components from snapping to absolute position on drag activation

--- src/commands/view/ComponentDrag.js
+++ src/commands/view/ComponentDrag.js
@@ -313,7 +313,7 @@ export default {
     const { center, onStart } = opts;
     const { Canvas } = editor;
     const style = target.getStyle();
-    const position = 'absolute';
+    const position = 'sticky';
     onStart && onStart(this._getDragData());
     if (isTran) return;
 
