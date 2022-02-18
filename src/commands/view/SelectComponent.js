b9d238a85be6cf08a2a234898d2eab802a6c0135
Josh Long <jlong@crestron.com>
2022-02-18 16:43:54 -0500

    ref: Additional checks before attempting to change css coord values resulting in NaN

--- src/commands/view/SelectComponent.js
+++ src/commands/view/SelectComponent.js
@@ -762,10 +762,16 @@ export default {
 
     let canvasLeftScroll = this.canvas.getFramesEl().parentNode.scrollLeft;
     let canvasTopScroll = this.canvas.getFramesEl().parentNode.scrollTop;
+    
+    let topCoordValue = parseFloat(canvasTopScroll);
+    let leftCoordValue = parseFloat(canvasLeftScroll);
 
-    style.top = `${parseFloat(canvasTopScroll) + parseFloat(topOff)}px`;
+    topCoordValue == NaN ? 0 + parseFloat(topOff) : topCoordValue + parseFloat(topOff);
+    leftCoordValue == NaN ? 0 + parseFloat(leftOff) : leftCoordValue + parseFloat(leftOff);
 
-    style.left = `${parseFloat(canvasLeftScroll) + parseFloat(leftOff)}px`;
+    style.top = `${topCoordValue}px`;
+
+    style.left = `${leftCoordValue}px`;
 
     style.width = pos.width + unit;
     style.height = pos.height + unit;
