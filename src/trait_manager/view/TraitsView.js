a69272e57886f682c16be80b691fa097344fd46c
Josh Long <jlong@crestron.com>
2022-02-25 18:01:49 -0500

    update to cache collection instead of using backbone 'get' on each iteration

--- src/trait_manager/view/TraitsView.js
+++ src/trait_manager/view/TraitsView.js
@@ -60,13 +60,14 @@ export default DomainViews.extend({
     const comp = this.em.getSelectedAll();
     // check if there are more than one component selected and get the last selected value (last elemented pushed)
     this.lastComp = comp.length > 0 ? comp[comp.length - 1] : undefined;
+    this.lastCompTraits = lastComp ? lastComp.get('traits') : undefined;
     this.el.className = `${this.className} ${ppfx}one-bg ${ppfx}two-color`;
     this.collection = {}; // object used as a map.
 
     comp.length &&
       comp.forEach(comp => {
         var self = this; // need to keep upper level scope
-        self.lastComp.get('traits').each(trait => {
+        self.lastCompTraits.each(trait => {
           var tta;
           if (
             (tta = comp.get('traits').findWhere({ name: trait.get('name') }))
