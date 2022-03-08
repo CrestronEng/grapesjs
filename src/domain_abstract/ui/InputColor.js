c315c053946b4a31f7ebfb6ffbe8b15bdb0b350d
Josh Long <jlong@crestron.com>
2022-03-08 18:19:39 -0500

    ref: addition of undo scope for color selection

--- src/domain_abstract/ui/InputColor.js
+++ src/domain_abstract/ui/InputColor.js
@@ -99,7 +99,14 @@ export default Input.extend({
           changed = 0;
           const cl = getColor(color);
           cpStyle.backgroundColor = cl;
-          model.setValueFromInput(cl, 0);
+          const undo = em.getEditor().Undo;
+          if (undo) {
+            undo.scope.usingUndoSuppressScope(() => {
+              model.setValueFromInput(cl, 0);
+            }, 100);
+          } else {
+            model.setValueFromInput(cl, 0);
+          }
         },
         change(color) {
           changed = 1;
