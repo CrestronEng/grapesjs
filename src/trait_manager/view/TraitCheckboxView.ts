23d0571044318173d3cc3dd8068aa9b2374f3aa5
rdixit <rdixit@crestron.com>
2026-02-07 01:46:57 +0530

    Reverted the change came from merge, it is not needed

--- src/trait_manager/view/TraitCheckboxView.ts
+++ src/trait_manager/view/TraitCheckboxView.ts
@@ -1,5 +1,5 @@
 import { isUndefined } from 'underscore';
-import TraitView from 'overrides/trait_manager/view/TraitView';
+import TraitView from './TraitView';
 
 export default class TraitCheckboxView extends TraitView {
   appendInput = false;
